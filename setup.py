eec501b31b796caf2c05d6c983c24265770c2ec3
Fantix King <fantix.king@gmail.com>
2019-02-17 10:46:34 -0600

    Prepare 1.1.0 for PyPI release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='gdcdictionary',
-    version='0.0.0',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
-        'PyYAML==3.11',
-        'jsonschema==2.5.1',
+        'PyYAML>=3.11',
+        'jsonschema>=2.5.1',
     ],
     package_data={
         "gdcdictionary": [
