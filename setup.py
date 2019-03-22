6d61d660e2fa28f810040c9d87cd204796d07723
Fantix King <fantix@uchicago.edu>
2019-03-22 15:31:13 -0500

    chore(yaml): upgrade dep versions

--- setup.py
+++ setup.py
@@ -5,12 +5,9 @@ setup(
     version='0.0.0',
     packages=find_packages(),
     install_requires=[
-        'PyYAML==3.11',
-        'jsonschema==2.5.1',
+        'PyYAML~=5.1',
+        'jsonschema>=2.5.1',
         'dictionaryutils',
-    ],
-    dependency_links=[
-       "git+https://github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils",
     ],
     package_data={
         "gdcdictionary": [
